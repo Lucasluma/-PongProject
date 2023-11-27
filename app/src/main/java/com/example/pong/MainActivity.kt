1d0adc2ec4b1b441ce95974aaa73447ebdfdd872
luma <lucas.martinahlm@gmail.com>
2023-11-27 15:01:53 +0100

    yes comment

--- app/src/main/java/com/example/pong/MainActivity.kt
+++ app/src/main/java/com/example/pong/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.pong
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//yes
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
